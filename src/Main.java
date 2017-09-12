5a4e14a350b90066f4f74268cd4365da3f23d376
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-09-12 13:59:36 -0500

    everything works

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,21 @@
 public class Main {
     public static void main(String[] args) {
 
-
         GamePiece test = new GamePiece();
 
-        System.out.println(test.getPositionX());
-        System.out.println(test.getPositionY());
+        test.setName("Super Awesome Game Piece");
         test.setColor("Red");
-        System.out.println(test.getColor());
-        System.out.println(test.freeze());
-        System.out.println(test.move(10, 200));
-        System.out.println(test.getPositionX());
-
+        System.out.println("Position X is: " + test.getPositionX());
+        System.out.println("Position Y is: " + test.getPositionY());
+        System.out.println("Is the piece frozen? " + test.isFrozen()); //should return false
+        System.out.println(test.freeze()); //should return "Freeze!"
+        System.out.println("Moving coordinates: " + test.move(10, 200));
+        System.out.println("Position X is: " + test.getPositionX()); //
+        System.out.println("Position Y is: " + test.getPositionY()); //should return 200
+        System.out.println("Is the piece frozen? " + test.isFrozen()); //should now return true
+        System.out.println(test.unfreeze()); //should return "It's melting!"
+        System.out.println("Moving coordinates: " + test.move(10, 200));
+        System.out.println("Position X is: " + test.getPositionX());
+        System.out.println("Position Y is: " + test.getPositionY());
     }
 }
