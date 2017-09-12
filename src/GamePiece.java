54d864c7c4d3dca3c2ecb2bd8ba4ed4bec0283fe
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-09-12 13:15:38 -0500

    game piece is modeled

---
+++ src/GamePiece.java
@@ -0,0 +1,43 @@
+public class GamePiece {
+    private int positionX;
+    private int positionY;
+    private boolean frozen;
+    private String name;
+    private String color;
+
+    public GamePiece() {
+        this.positionX = 0;
+        this.positionY = 0;
+        this.frozen = false;
+        this.name = name;
+        this.color = color;
+    }
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public boolean isFrozen() {
+        return frozen;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
